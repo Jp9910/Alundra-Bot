58240ecb5cab6d06e3306d277d46f375c56bc202
Joao Secundo <alundra@academico.ufs.br>
2022-07-19 01:07:18 -0300

    Reply based on message content

--- src/bot.php
+++ src/bot.php
@@ -19,12 +19,23 @@ $discord->on('ready', function (Discord $discord) {
 
     // Listen for messages.
     $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        if ($message->author->bot){
+        if ($message->author->bot) {
             return;
         }
-        $message->reply('Resposta!');
+        // var_dump($message);
+        switch ($message->content) {
+            case 'pog':
+                $message->reply("PogChamp");
+                break;
+            case 'oi':
+                $message->reply("Oi {$message->author->username}!!");
+                break;
+        }
         echo "{$message->author->username}: {$message->content}", PHP_EOL;
     });
 });
 
 $discord->run();
+
+// Events: https://discord-php.github.io/DiscordPHP/reference/classes/Discord-WebSockets-Event.html
+// Message class: https://discord-php.github.io/DiscordPHP/reference/classes/Discord-Parts-Channel-Message.html#property_channel_id
